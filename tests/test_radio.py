c826bd59a56cad1dcec4b1a49c30218c2918d87f
Theo Hill <Theo0x48@gmail.com>
2024-09-21 20:27:58 -0700

    Check TX doppler in unit test
    And also verify that both dopplers pass their respective center
    frequencies.

--- tests/test_radio.py
+++ tests/test_radio.py
@@ -8,11 +8,14 @@ from pass_commander.Radio import Radio
 from pass_commander.Tracker import Tracker
 from .mock_flowgraph import Flowgraph
 
+
 class test_radio(unittest.TestCase):
     def test_doppler(self):
         flowgraph = Flowgraph()
         rx = []
+        tx = []
         flowgraph.server.register_function(lambda x: rx.append(x), "set_gpredict_rx_frequency")
+        flowgraph.server.register_function(lambda x: tx.append(x), "set_gpredict_tx_frequency")
         fgthread = Thread(target=flowgraph.start)
         fgthread.start()
 
@@ -23,19 +26,24 @@ class test_radio(unittest.TestCase):
                 "2 52017  97.4861 255.7395 0002474 307.8296  52.2743 15.72168729136382",
             ]
         })
-        date = ephem.Date(45541.170401489704) # start of a pass, determined through divination
+        date = ephem.Date(45541.170401489704)  # start of a pass, determined through divination
         track.freshen(date)
         radio = Radio("127.0.0.2")
 
-        for t in range(10*60): # passes are about 10 minutes
+        for t in range(10 * 60):  # passes are about 10 minutes
             track.freshen(ephem.Date(date + t * ephem.second))
             radio.set_rx_frequency(track)
+            radio.set_tx_frequency(track)
 
         flowgraph.stop()
         fgthread.join()
         radio.close()
 
-        # doppler moves from high frequency to low frequency
-        self.assertGreater(rx[0], rx[-1])
+        # RX doppler moves from high frequency to low frequency, passing over the center frequency
+        self.assertTrue(rx[0] > radio.rxfreq > rx[-1])
         # monotonically decreasing
         self.assertTrue(all(x > y for x, y in pairwise(rx)))
+
+        # TX does the opposite, monotonically low -> high, again passing the center frequency
+        self.assertTrue(tx[0] < radio.txfreq < tx[-1])
+        self.assertTrue(all(x < y for x, y in pairwise(tx)))
